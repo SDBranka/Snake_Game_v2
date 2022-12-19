3c9be2ee085f5eb56969abeb169b99a4b5820d28
Sam Branka <sdbranka@me.com>
2022-12-19 14:20:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Snake Game V2
+
+##### Table of Contents
+
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+
+---
+
+<p float="center">
+    <img src="https://github.com/SDBranka/Snake_Game/blob/main/Resources/Screenshot%202022-11-18%20120357.png" width=45% alt="gameplay image"/>
+</p>
+
+## Description
+
+Version 2 of the Snake Game. 
+<ul>
+<li>Snake can move around the screen</li>
+<li>Food is produced and can be consumed
+<ul><li>If food is consumed<ul><li>Snake grows by one segment</li><li>Score increases by one</li></ul></li></ul></li>
+
+##### Controls
+
+- The UP keyboard button will turn the snake North 
+- The DOWN keyboard button will turn the snake South
+- The RIGHT keyboard button will turn the snake East
+- The LEFT keyboard button will turn the snake West 
+
+*Note: The snake cannot turn back upon itself
+
+##### Technologies
+
+- Python
+- Turtle module
+- Visual Studio
+
+---
+
+## How To Use
+
+Download or clone this repository to your desktop. Run main.py in an appropriate Python environment.
+
+---
+
+## References
+
+##### Continuing Work on
+- https://github.com/SDBranka/Snake_Game
+- https://github.com/SDBranka/_100DOP_Exercises
+
+\
+[Back To The Top](#snake-game)
---
+++ main.py
@@ -0,0 +1,70 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+# build screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+# set tracer to 0 to enable using the update method
+screen.tracer(0)
+
+# create scoreboard
+scoreboard = Scoreboard()
+# create snake
+snake = Snake()
+# create food
+food = Food()
+
+# establish key listeners
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# main game loop
+game_is_on = True
+while game_is_on:
+    # print(scoreboard.message + str(scoreboard.score))
+
+    # updates the screen after each move
+    # placed outside of the move loop
+    # prevents seeing each independent segment 
+    # move individually
+    screen.update()
+    # delays the time cycle of 
+    # each move by 0.1 second
+    time.sleep(0.1)
+
+    snake.move()
+
+    # detect snake collision with food
+    if snake.head.distance(food) < 20:
+        # console test statement
+        # print("Snake ate the food")
+        
+        # food respawns to new location
+        food.refresh()
+        # snake grows corresponding to food consumed
+        snake.extend()
+        # update scoreboard
+        scoreboard.increase_score()
+
+    # detect snake collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.game_over()
+        game_is_on = False
+
+    # detect collision of snake with itself
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
+
+
+screen.exitonclick()
