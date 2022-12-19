d59035dfbc5f906a9a4352efeacb784a9645a47f
Sam Branka <sdbranka@me.com>
2022-12-19 15:38:27 -0500

    snake updated

--- snake.py
+++ snake.py
@@ -15,9 +15,11 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
+
 
     def add_segment(self, position):
         new_segment = Turtle("square")
@@ -25,6 +27,7 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
@@ -37,17 +40,21 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
 
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
 
     def right(self):
         if self.head.heading() != LEFT:
